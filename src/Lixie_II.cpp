244659bbb8847807970d3ac829af3390b1f30619
Lixie Labs <connornishijima@gmail.com>
2019-11-24 02:02:47 -0700

    Manual brightness enforcement

--- src/Lixie_II.cpp
+++ src/Lixie_II.cpp
@@ -1,32 +1,4 @@
-#include <bitswap.h>
-#include <chipsets.h>
-#include <color.h>
-#include <colorpalettes.h>
-#include <colorutils.h>
-#include <controller.h>
-#include <cpp_compat.h>
-#include <dmx.h>
-#include <FastLED.h>
-#include <fastled_config.h>
-#include <fastled_delay.h>
-#include <fastled_progmem.h>
-#include <fastpin.h>
-#include <fastspi.h>
-#include <fastspi_bitbang.h>
-#include <fastspi_dma.h>
-#include <fastspi_nop.h>
-#include <fastspi_ref.h>
-#include <fastspi_types.h>
-#include <hsv2rgb.h>
-#include <led_sysdefs.h>
-#include <lib8tion.h>
-#include <noise.h>
-#include <pixelset.h>
-#include <pixeltypes.h>
-#include <platforms.h>
-#include <power_mgt.h>
-
-    /*
+/*
   Lixie_II.cpp - Library for controlling the Lixie 2!
   
   Created by Connor Nishijma July 6th, 2019
